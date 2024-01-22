6d39868ab36d5820505f033ed1fd5726161bc5ca
ucupWork <work.ysetiawan@gmail.com>
2024-01-22 16:45:12 +0700

    layout home

--- components/CardDataStats.tsx
+++ components/CardDataStats.tsx
@@ -1,37 +1,37 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode } from 'react'
 
 interface CardDataStatsProps {
-  title: string;
-  total: string;
-  rate: string;
-  levelUp?: boolean;
-  levelDown?: boolean;
-  children: ReactNode;
+  title: string
+  total: string
+  // rate: string;
+  // levelUp?: boolean;
+  // levelDown?: boolean;
+  children: ReactNode
 }
 
 const CardDataStats: React.FC<CardDataStatsProps> = ({
   title,
   total,
-  rate,
-  levelUp,
-  levelDown,
-  children,
+  // rate,
+  // levelUp,
+  // levelDown,
+  children
 }) => {
   return (
-    <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
-      <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
+    <div className='rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark'>
+      <div className='flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4'>
         {children}
       </div>
 
-      <div className="mt-4 flex items-end justify-between">
+      <div className='mt-4 flex items-end justify-between'>
         <div>
-          <h4 className="text-title-md font-bold text-black dark:text-white">
+          <h4 className='text-title-md font-bold text-black dark:text-white'>
             {total}
           </h4>
-          <span className="text-sm font-medium">{title}</span>
+          <span className='text-sm font-medium'>{title}</span>
         </div>
 
-        <span
+        {/* <span
           className={`flex items-center gap-1 text-sm font-medium ${
             levelUp && 'text-meta-3'
           } ${levelDown && 'text-meta-5'} `}
@@ -68,10 +68,10 @@ const CardDataStats: React.FC<CardDataStatsProps> = ({
               />
             </svg>
           )}
-        </span>
+        </span> */}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CardDataStats;
+export default CardDataStats
